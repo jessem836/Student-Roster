111d607616682bb3e76ec2d56fc460aafc4308df
Jesse Mota <jessemota836@gmail.com>
2024-10-24 15:01:48 -0700

    Setting up variables withing degree.h and student.h

--- degree.h
+++ degree.h
@@ -1,5 +1,17 @@
 #pragma once
+#ifndef DEGREE_H
+#define DEGREE_H
+
 class degree
 {
+
+public:
+
+private:
+
+	enum degreeProgram {SECURITY, NETWORK, SOFTWARE};
+
 };
+
+#endif
 
--- student.h
+++ student.h
@@ -1,5 +1,19 @@
 #pragma once
+using std::string;
+#ifndef STUDENT_H
+#define STUDENT_H
+
 class student
 {
+public:
+
+private:
+	string firstName;
+	string lastName;
+	string emailAddress;
+	int age;
+	int courseDays[3];
+	string degreeProgram;
 };
 
+#endif
